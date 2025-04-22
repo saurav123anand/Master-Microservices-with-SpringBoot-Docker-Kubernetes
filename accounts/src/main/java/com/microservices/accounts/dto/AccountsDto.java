38023d438219a29e72196b8a290a148faf215cb1
saurav123anand <123sauravanand@gmail.com>
2025-04-23 04:18:09 +0530

    SWAGGER Implementation code added

--- accounts/src/main/java/com/microservices/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/microservices/accounts/dto/AccountsDto.java
@@ -1,5 +1,6 @@
 package com.microservices.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -11,12 +12,25 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
     @NotEmpty(message = "AccountNumber can not be a null or empty")
     @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    @Schema(
+            description = "Account Number of Phoenix Bank account", example = "3454433243"
+    )
     private Long accountNumber;
+    @Schema(
+            description = "Account type of Phoenix Bank account", example = "Savings"
+    )
     @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
+    @Schema(
+            description = "Phoenix Bank branch address", example = "123 NewYork"
+    )
     @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 }
