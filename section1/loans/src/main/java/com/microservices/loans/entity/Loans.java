4f3ce28d617827cce6ab6106914fc2e169fc92b8
saurav123anand <123sauravanand@gmail.com>
2025-05-04 01:22:59 +0530

    code added

---
+++ section1/loans/src/main/java/com/microservices/loans/entity/Loans.java
@@ -0,0 +1,28 @@
+package com.microservices.loans.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Loans extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanId;
+
+    private String mobileNumber;
+
+    private String loanNumber;
+
+    private String loanType;
+
+    private int totalLoan;
+
+    private int amountPaid;
+
+    private int outstandingAmount;
+}
