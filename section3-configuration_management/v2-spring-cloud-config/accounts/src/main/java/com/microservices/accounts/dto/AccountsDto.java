a68ca239b2f8bb589cbd788a3e3da186aae64b41
saurav123anand <123sauravanand@gmail.com>
2025-05-18 05:14:19 +0530

    code added

---
+++ section3-configuration_management/v2-spring-cloud-config/accounts/src/main/java/com/microservices/accounts/dto/AccountsDto.java
@@ -0,0 +1,36 @@
+package com.microservices.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
+public class AccountsDto {
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    @Schema(
+            description = "Account Number of Phoenix Bank account", example = "3454433243"
+    )
+    private Long accountNumber;
+    @Schema(
+            description = "Account type of Phoenix Bank account", example = "Savings"
+    )
+    @NotEmpty(message = "AccountType can not be a null or empty")
+    private String accountType;
+    @Schema(
+            description = "Phoenix Bank branch address", example = "123 NewYork"
+    )
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
+    private String branchAddress;
+}
---
+++ section3-configuration_management/v2-spring-cloud-config/loans/src/main/java/com/microservices/loans/LoansApplication.java
@@ -0,0 +1,41 @@
+package com.microservices.loans;
+
+import com.microservices.loans.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "PhoenixBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Saurav J.",
+						email = "example@gmail.com",
+						url = "https://www.example.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.example.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "PhoenixBank Loans microservice REST API Documentation",
+				url = "https://www.example.com/swagger-ui.html"
+		)
+)
+public class LoansApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+}
